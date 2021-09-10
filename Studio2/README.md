e0cd40a78bc9e7d41416709f5ab86b25943827db
boanderson <46663503+boanderson@users.noreply.github.com>
2021-09-10 11:26:55 -0500

    studio2 finish

---
+++ Studio2/README.md
@@ -0,0 +1,4 @@
+Studio 2
+
+Bo Anderson
+Partner: Marissa Kalkar
--- Studio2/studio2-template/js/studio2.js
+++ Studio2/studio2-template/js/studio2.js
@@ -9,11 +9,27 @@ function dataFiltering() {
 
 	/* **************************************************
 	 *
-	 * ADD YOUR CODE HERE (ARRAY/DATA MANIPULATION)
+	//sort JSON array descending order by visitors
+	*/
+	attractions.sort(function(a,b) {
+		return b.Visitors - a.Visitors;
+	});
+
+	var filtered = attractions.filter(function(value, index) {
+		return (index < 5);
+	});
+
+	attractions = filtered;
+	/*
 	 *
 	 * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
-	 *
-	 * renderBarChart(data)
+	 */
+		
+	renderBarChart(attractions);
+
+	//console.log(attractions);
+
+	/*
 	 *
 	 * - 'data' must be an array of JSON objects
 	 * - the max. length of 'data' is 5
